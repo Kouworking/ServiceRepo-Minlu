9eb68003dcbf7e90e3dafd76d0fc106b68f580d7
lulu2233 <ninglv111@gmail.com>
2020-03-11 15:29:33 -0700

    create a basic mysql schema

--- database/index.js
+++ database/index.js
@@ -1,16 +1,10 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+const mysql = require('mysql');
+const mysqlConfig = require('./config.js');
 
-let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
-});
+const connection = mysql.createConnection(mysqlConfig, {multipleStatements: true});
 
-let Repo = mongoose.model('Repo', repoSchema);
+//could sort by frequent of category
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
-}
+module.exports = {
 
-module.exports.save = save;
+};
---
+++ schema.sql
@@ -0,0 +1,38 @@
+DROP DATABASE IF EXISTS bookingSys;
+
+CREATE DATABASE bookingSys;
+
+USE bookingSys;
+
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  name VARCHAR,
+  activity VARCHAR,
+  count int,
+  email VARCHAR,
+  ifAlcoho BOOLEAN,
+  PRIMARY KEY (ID)
+);
+
+-- create a new table for categories
+CREATE TABLE reservations(
+  id int NOT NULL AUTO_INCREMENT,
+  date Date,
+  time time,
+  PRIMARY KEY (ID)
+);
+
+
+/*  Execute this file from the command line by typing:
+ *    mysql -u <USER> < schema.sql
+ *    OR
+ *    mysql -u <USER> -p < schema.sql
+ *  For example, on a pairing station, it'll be
+ *    mysql -u student -p < schema.sql
+ *  and then you'll have to enter the password, student
+ *  On your personal computer, if you haven't set up
+ *  a password, it'll be
+ *    mysql -u root < schema.sql
+*/
+
+
